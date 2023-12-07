76dfaa2c1ee64b972c9cb8f268a3b7f6065aef63
James Gao <jamessmgao@berkeley.edu>
2023-12-07 07:59:05 +0000

    added route

--- spec/controllers/representatives_controller_spec.rb
+++ spec/controllers/representatives_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RepresentativesController, type: :controller do
   describe "GET #show" do
     it 'renders the show template' do
-      representative = Representative.create(name: 'Willie James')
+      representative = Representative.create(name: 'Willie James', ocd_id: '162', title: 'Governor')
       get :show, params: { id: representative.id }
       expect(response).to render_template(:show)
     end
