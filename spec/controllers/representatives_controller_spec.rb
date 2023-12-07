a56ab0c1f38689b848f0e5b6f698acaae72b6b8d
James Gao <jamessmgao@berkeley.edu>
2023-12-07 07:48:32 +0000

    error

--- spec/controllers/representatives_controller_spec.rb
+++ spec/controllers/representatives_controller_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe RepresentativesController, type: :controller do
   describe "GET #show" do
-    let(:representative) { create(:representative) } # Adjust as per your factory setup
-
-    it "renders the show template" do
+    it 'renders the show template' do
+      representative = Representative.create(name: 'Willie James')
       get :show, params: { id: representative.id }
       expect(response).to render_template(:show)
     end
