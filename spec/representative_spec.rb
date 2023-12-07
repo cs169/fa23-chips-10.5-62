713e2f7292dfb33747172668c016a8ba314176ec
James Gao <jamessmgao@berkeley.edu>
2023-12-07 06:51:19 +0000

    added more test cases erroring

--- spec/representative_spec.rb
+++ spec/representative_spec.rb
@@ -6,12 +6,20 @@ RSpec.describe Representative, type: :model do
       let!(:existing_rep) { Representative.create(ocd_id: '162', name: 'Evan Sukhpal', title: 'Senator')}
 
       it 'updates existing representative' do
-        params = { ocd_id: '123', name: 'Jane Doe', title: 'Senator' }
+        params = { ocd_id: '162', name: 'Jane Doe', title: 'Senator' }
 
         representative = Representative.civic_api_to_representative_params(params)
         expect(representative.id).to eq existing_rep.id
         expect(representative.name).to eq 'Jane Doe'
-        expect(representative.count).to eq 1
+        expect(Representative.count).to eq 1
+      end
+
+      it 'updates specified fields' do
+        params = { ocd_id: '162', title: 'Governor' }
+
+        representative = Representative.civic_api_to_representative_params(params)
+        expect(representative.title).to eq 'Governor'
+        expect(representative.name).to eq 'Evan Sukhpal'
       end
     end
 
@@ -19,6 +27,13 @@ RSpec.describe Representative, type: :model do
       it 'creates new representative' do
         params = { ocd_id: '261', name: 'Laksith James', title: 'Congressman'}
         expect {Representative.civic_api_to_representative_params(params)}.to change(Representative, :count).by(1)
+      end
+    end
+
+    context 'with invalid data' do 
+      it 'does not create a rep with invalid data' do
+        invalid_params = {ocd_id: '', name: '', title: ''}
+        expect {Representative.civic_api_to_representative_params(invalid_params)}.not_to change(Representative, :count)
       end
     end
   end
