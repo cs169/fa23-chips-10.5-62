143e72a8af877cb60eed62afa6654f2cbf81d2be
James Gao <jamessmgao@berkeley.edu>
2023-12-09 07:25:25 +0000

    db add_columns change

--- db/migrate/20200728065604_add_columns_to_representatives.rb
+++ db/migrate/20200728065604_add_columns_to_representatives.rb
@@ -2,10 +2,10 @@
 
 class AddColumnsToRepresentatives < ActiveRecord::Migration[5.2]
   def change
-    add_column :representatives, :ocdid, :string unless column_exists?(:representatives, :ocdid)
-    add_column :representatives, :title, :string unless column_exists?(:representatives, :title)
-    add_column :representatives, :address, :string unless column_exists?(:representatives, :address)
-    add_column :representatives, :party, :string unless column_exists?(:representatives, :party)
-    add_column :representatives, :photo, :string unless column_exists?(:representatives, :photo)
+    add_column :representatives, :ocdid, :string 
+    add_column :representatives, :title, :string
+    add_column :representatives, :address, :string 
+    add_column :representatives, :party, :string 
+    add_column :representatives, :photo, :string 
   end
 end
