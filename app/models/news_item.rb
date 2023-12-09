81e95d3e576eb02f6ce860dfbe3f7b5b4f24a967
James Gao <jamessmgao@berkeley.edu>
2023-12-09 04:58:49 +0000

    added all but changing views for _form

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -4,6 +4,14 @@ class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
 
+  issues = ["Free Speech", "Immigration", "Terrorism", "Social Security and
+  Medicare", "Abortion", "Student Loans", "Gun Control", "Unemployment",
+  "Climate Change", "Homelessness", "Racism", "Tax Reform", "Net
+  Neutrality", "Religious Freedom", "Border Security", "Minimum Wage",
+  "Equal Pay"]
+
+  validates :issue, presence: true, inclusion: { in: issues }
+
   def self.find_for(representative_id)
     NewsItem.find_by(
       representative_id: representative_id
