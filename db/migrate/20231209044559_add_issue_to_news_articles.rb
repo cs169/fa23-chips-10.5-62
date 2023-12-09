8393f5a6a05f827841a707360c24d4f5ceb536ce
James Gao <jamessmgao@berkeley.edu>
2023-12-09 05:17:46 +0000

    db migration error

--- db/migrate/20231209044559_add_issue_to_news_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddIssueToNewsArticles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :news_articles, :issue, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_065604) do
+ActiveRecord::Schema.define(version: 2020_06_16_205245) do
 
   create_table "counties", force: :cascade do |t|
     t.string "name", null: false
