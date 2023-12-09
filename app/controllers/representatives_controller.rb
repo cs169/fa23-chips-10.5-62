5a1b999f8ef6c6caf67e86ac656be624439c7e8c
James Gao <jamessmgao@berkeley.edu>
2023-12-09 02:39:47 +0000

    working on 1.1

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,4 +4,17 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+
+  def show
+    @representative = Representative.find(params[:id])
+
+    
+    @name = @representative.name
+    @title = @representative.title
+    @ocd_id = @representative.ocd_id
+    @party = @representative.party
+    @photo = @representative.photo
+
+    render 'representatives/show'
+  end
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,26 +2,40 @@
 
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
+  validates :name, presence: true
 
   def self.civic_api_to_representative_params(rep_info)
     reps = []
 
-    rep_info.officials.each_with_index do |official, index|
-      ocdid_temp = ''
-      title_temp = ''
-
-      rep_info.offices.each do |office|
-        if office.official_indices.include? index
-          title_temp = office.name
-          ocdid_temp = office.division_id
-        end
-      end
-
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
-      reps.push(rep)
+    rep_info.officials.each do |official|
+      data = extract_rep_data(official, rep_info)
+      rep = Representative.find_or_initialize_by(name: official.name)
+      rep.update(data)
+      reps.push(rep) if rep.persisted?
     end
 
     reps
+  end
+
+  def self.office_info(official, rep_info)
+    rep_info.offices.each do |office|
+      if office.official_indices.include?(official.index)
+        return [office.name, office.division_id]
+      end
+    end
+    ['', '']
+  end
+
+  def self.extract_rep_data(official, info)
+    title, ocd_id = office_info(official, info)
+    address = format_address(official.address)
+
+    { name: official.name, ocd_id: ocd_id, title: title, address: address, party: official.party, photo: official.photo_url }
+  end
+
+  def self.format_address(info)
+    return '' if info.nil? || info.empty?
+    address = info.first
+    [address.line1, address.city, address.state, address.zip].join(' ')
   end
 end
